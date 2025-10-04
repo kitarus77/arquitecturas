293786877444722bbcdff33e549572585fbe0ccc
PC-OSCAR\oskar <oscar.mendez.tierra@gmail.com>
2025-10-04 11:26:24 +0200

    First commit

---
+++ src/test/java/com/kitarsoft/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kitarsoft;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("LOCAL")
+class ApplicationTests {
+
+	//@Test
+	void contextLoads() {
+	}
+
+}
